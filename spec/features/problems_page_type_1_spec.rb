8af602bb59ef259627f418c8c612e4e2b2dba765
kevin <kjs3767@tamu.edu>
2025-01-21 13:13:15 -0600

    rubocop offense resolved

--- spec/features/problems_page_type_1_spec.rb
+++ spec/features/problems_page_type_1_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Problem-type 1 Page", type: :feature do
     visit problem_type_1_path
 
     expect(page).to have_selector("h1", text: "Problem-type 1")
-    
+
     expect(page).to have_button("Back home")
     click_button "Back home"
     expect(current_path).to eq(student_home_path)
