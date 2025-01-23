b105a418f98021f5d8a72570ccdaafaf03917b22
Nicholas Turoci <n2rowc@tamu.edu>
2025-01-23 23:10:56 +0000

    Fixed rubocop offenses

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,7 +63,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  # 
+  #
   SimpleCov.start 'rails' do
     add_filter '/spec/'
   end
