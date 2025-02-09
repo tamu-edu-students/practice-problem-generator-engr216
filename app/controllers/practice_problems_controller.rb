9dad252e90dac3855c630e3dbae9c012951f0fda
Dhruv Manihar <dhruvmanihar@gmail.com>
2025-02-09 17:16:51 +0000

    Fixed rubocop issues

--- app/controllers/practice_problems_controller.rb
+++ app/controllers/practice_problems_controller.rb
@@ -7,20 +7,18 @@ class PracticeProblemsController < ApplicationController
   def generate
     @category = Category.find(params[:category_id])
     all_questions = ProblemGenerator.new(@category).generate_questions
-    
+
     # If there is a stored problem and there’s more than one option, filter it out.
-    if session[:last_question] && all_questions.size > 1
-      filtered_questions = all_questions.reject { |q| q[:question] == session[:last_question] }
-    else
-      filtered_questions = all_questions
-    end
-    
+    filtered_questions = if session[:last_question] && all_questions.size > 1
+                           all_questions.reject { |q| q[:question] == session[:last_question] }
+                         else
+                           all_questions
+                         end
+
     @question = filtered_questions.sample
-    
+
     # Store the current question so that next time it can be filtered out.
     session[:last_question] = @question[:question]
     render :generate
   end
-  
-
 end
