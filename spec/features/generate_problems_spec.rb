8d70d21205b716b7d3fdc90bb0288411243aef25
Dhruv Manihar <dhruvmanihar@gmail.com>
2025-02-09 17:08:05 +0000

    Rspec tests working

---
+++ spec/features/generate_problems_spec.rb
@@ -0,0 +1,41 @@
+# spec/features/generate_problems_spec.rb
+require 'rails_helper'
+
+RSpec.describe "practice_problems/generate.html.erb", type: :view do
+  before do
+    @category = Category.create!(name: "Measurement & Error")
+    @question = {
+      question: "What is Newton's second law?",
+      choices: ['F = ma', 'E = mc^2', 'P = IV', 'V = IR']
+    }
+    assign(:category, @category)
+    assign(:question, @question)
+    render
+  end
+
+  it "displays the navbar text" do
+    expect(rendered).to include("Past Problems")
+  end
+
+  it "displays the category title" do
+    expect(rendered).to have_content("Category: #{@category.name}")
+  end
+
+  it "displays the question text" do
+    expect(rendered).to have_content("Question: #{@question[:question]}")
+  end
+
+  it "displays each answer choice" do
+    @question[:choices].each do |choice|
+      expect(rendered).to include(choice)
+    end
+  end
+
+  it "displays a Change Category link" do
+    expect(rendered).to include("Change Category")
+  end
+
+  it "displays a Generate New Problem link" do
+    expect(rendered).to include("Generate New Problem")
+  end
+end
