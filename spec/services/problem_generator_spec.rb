5d631381762d0a31e0e281d4d35a6fe06f819ee0
Dhruv Manihar <dhruvmanihar@gmail.com>
2025-02-12 20:04:23 +0000

    Test coverage and rubocop

---
+++ spec/services/problem_generator_spec.rb
@@ -0,0 +1,36 @@
+# spec/services/problem_generator_spec.rb
+require 'rails_helper'
+
+RSpec.describe ProblemGenerator, type: :service do
+  let(:category) { Category.create!(name: 'Physics') }
+  let(:problem_generator) { described_class.new(category) }
+
+  describe '#initialize' do
+    it 'assigns the category' do
+      expect(problem_generator.instance_variable_get(:@category)).to eq(category)
+    end
+  end
+
+  describe '#generate_questions' do
+    it 'returns an array of questions' do
+      expect(problem_generator.generate_questions).to be_an(Array)
+    end
+
+    it 'returns the expected dummy questions' do
+      expect(problem_generator.generate_questions).to match_array(described_class::QUESTIONS)
+    end
+
+    it 'each question has question, choices, and answer keys' do
+      problem_generator.generate_questions.each do |q|
+        expect(q).to include(:question, :choices, :answer)
+      end
+    end
+  end
+
+  describe 'QUESTIONS constant' do
+    it 'is frozen (immutable)' do
+      expect { described_class::QUESTIONS << { question: 'New Q?', choices: [], answer: '' } }
+        .to raise_error(FrozenError)
+    end
+  end
+end
