84d481b47f4ad19275fd48c0ec874ad557eff670
Nicholas Turoci <n2rowc@tamu.edu>
2025-04-16 04:06:46 +0000

    rubo

--- app/modules/universal_account_equations_problem_generators.rb
+++ app/modules/universal_account_equations_problem_generators.rb
@@ -1,7 +1,7 @@
 # Module containing all UAE problem generators
 module UniversalAccountEquationsProblemGenerators
   # Create the basic structure for all UAE problems
-  def build_uae_problem(question_text, answer, params = {}, template_id)
+  def build_uae_problem(question_text, answer, template_id, params = {})
     {
       template_id: template_id,
       type: 'universal_account_equations',
@@ -43,12 +43,12 @@ module UniversalAccountEquationsProblemGenerators
     velocity = Math.sqrt(velocity_squared)
 
     question_text = roller_coaster_velocity_question_text(mass, height, distance, drag_force)
-    build_uae_problem(question_text, velocity, {
+    build_uae_problem(question_text, velocity, 1, {
                         mass: mass,
                         height: height,
                         distance: distance,
                         drag_force: drag_force
-                      }, 1)
+                      })
   end
 
   def generate_jam_production_cost_problem
@@ -73,7 +73,7 @@ module UniversalAccountEquationsProblemGenerators
       final_solids_percent, blackberry_cost, sugar_cost,
       target_jam_mass
     )
-    build_uae_problem(question_text, blackberry_cost_total, {
+    build_uae_problem(question_text, blackberry_cost_total, 2, {
                         blackberry_ratio: blackberry_ratio,
                         sugar_ratio: sugar_ratio,
                         blackberry_water_percent: blackberry_water_percent,
@@ -83,7 +83,7 @@ module UniversalAccountEquationsProblemGenerators
                         blackberry_cost: blackberry_cost,
                         sugar_cost: sugar_cost,
                         target_jam_mass: target_jam_mass
-                      }, 2)
+                      })
   end
 
   def generate_fuel_efficiency_problem
@@ -106,14 +106,14 @@ module UniversalAccountEquationsProblemGenerators
       vehicle_mass,
       rolling_resistance
     )
-    build_uae_problem(question_text, efficiency, {
+    build_uae_problem(question_text, efficiency, 3, {
                         distance: distance,
                         fuel_used: fuel_used,
                         fuel_cost: fuel_cost,
                         wind_resistance: wind_resistance,
                         vehicle_mass: vehicle_mass,
                         rolling_resistance: rolling_resistance
-                      }, 3)
+                      })
   end
 
   def generate_mixing_solution_problem
@@ -141,7 +141,7 @@ module UniversalAccountEquationsProblemGenerators
       temperature,
       mixing_time
     )
-    build_uae_problem(question_text, final_concentration, {
+    build_uae_problem(question_text, final_concentration, 4, {
                         solution_a_volume: solution_a_volume,
                         solution_a_concentration: solution_a_concentration,
                         solution_b_volume: solution_b_volume,
@@ -149,7 +149,7 @@ module UniversalAccountEquationsProblemGenerators
                         evaporation_rate: evaporation_rate,
                         temperature: temperature,
                         mixing_time: mixing_time
-                      }, 4)
+                      })
   end
 
   def generate_simple_interest_problem
@@ -172,14 +172,14 @@ module UniversalAccountEquationsProblemGenerators
       tax_rate,
       compounding_frequency
     )
-    build_uae_problem(question_text, interest, {
+    build_uae_problem(question_text, interest, 5, {
                         principal: principal,
                         rate: rate,
                         time: time,
                         inflation_rate: inflation_rate,
                         tax_rate: tax_rate,
                         compounding_frequency: compounding_frequency
-                      }, 5)
+                      })
   end
 
   def generate_electricity_bill_problem
@@ -205,7 +205,7 @@ module UniversalAccountEquationsProblemGenerators
       peak_hour_multiplier,
       peak_hours_per_day
     )
-    build_uae_problem(question_text, total_cost, {
+    build_uae_problem(question_text, total_cost, 6, {
                         power_rating: power_rating,
                         hours_per_day: hours_per_day,
                         days: days,
@@ -213,7 +213,7 @@ module UniversalAccountEquationsProblemGenerators
                         efficiency_factor: efficiency_factor,
                         peak_hour_multiplier: peak_hour_multiplier,
                         peak_hours_per_day: peak_hours_per_day
-                      }, 6)
+                      })
   end
 
   def generate_weight_conversion_problem
@@ -241,13 +241,13 @@ module UniversalAccountEquationsProblemGenerators
       num_items, weight_per_item, shipping_cost_per_kg,
       packaging_weight_percent, fragile_item_multiplier, fragile_items_percent
     )
-    build_uae_problem(question_text, total_cost, {
+    build_uae_problem(question_text, total_cost, 7, {
                         num_items: num_items,
                         weight_per_item: weight_per_item,
                         shipping_cost_per_kg: shipping_cost_per_kg,
                         packaging_weight_percent: packaging_weight_percent,
                         fragile_item_multiplier: fragile_item_multiplier,
                         fragile_items_percent: fragile_items_percent
-                      }, 7)
+                      })
   end
 end
