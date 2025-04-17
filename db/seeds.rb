b394144d1e7c9639089996a259b9d9411c7e6a12
Nicholas Turoci <n2rowc@tamu.edu>
2025-04-17 03:06:56 +0000

    Added seeding changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,65 +1,65 @@
 require 'faker'
 
 # Clear existing data for Students, Teachers, Questions, and Answers
-# Semester.destroy_all
-# Student.destroy_all
-# Teacher.destroy_all
-# Question.destroy_all
-# Answer.destroy_all
+Semester.destroy_all
+Student.destroy_all
+Teacher.destroy_all
+Question.destroy_all
+Answer.destroy_all
 
-# # Reset SQLite autoincrement during testing in development
-# ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='students'")
-# ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='teachers'")
-# ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='questions'")
-# ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='answers'")
+# Reset SQLite autoincrement during testing in development
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='students'")
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='teachers'")
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='questions'")
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='answers'")
 
 # Seed Semesters
-# semesters_data = [
-#   { name: 'Spring 2023', active: false },
-#   { name: 'Fall 2023', active: false },
-#   { name: 'Spring 2024', active: true },
-#   { name: 'Summer 2024', active: true },
-#   { name: 'Fall 2024', active: true },
-#   { name: 'Spring 2025', active: true }
-# ]
+semesters_data = [
+  { name: 'Spring 2023', active: false },
+  { name: 'Fall 2023', active: false },
+  { name: 'Spring 2024', active: true },
+  { name: 'Summer 2024', active: true },
+  { name: 'Fall 2024', active: true },
+  { name: 'Spring 2025', active: true }
+]
 
-# semesters = semesters_data.map do |semester_data|
-#   Semester.create!(semester_data)
-# end
+semesters = semesters_data.map do |semester_data|
+  Semester.create!(semester_data)
+end
 
-# # Seed Teachers
-# teachers_data = [
-#   { email: 'test_teacher@tamu.edu', first_name: 'test', last_name: 'teacher' },
-#   { email: 'kjs3767@tamu.edu', first_name: 'kevin', last_name: 'shi' },
-#   { email: 'n2rowc@tamu.edu', first_name: 'nicholas', last_name: 'tuorci' },
-#   { email: 'coopercalk@tamu.edu', first_name: 'cooper', last_name: 'calk' },
-#   { email: 'jordandary@tamu.edu', first_name: 'jordan', last_name: 'daryanani' },
-#   { email: 'vivek.somarapu@tamu.edu', first_name: 'vivek', last_name: 'somarapu' },
-#   { email: 'dhruvmanihar@tamu.edu', first_name: 'dhruv', last_name: 'manihar' }
-# ]
+# Seed Teachers
+teachers_data = [
+  { email: 'test_teacher@tamu.edu', first_name: 'test', last_name: 'teacher' },
+  { email: 'kjs3767@tamu.edu', first_name: 'kevin', last_name: 'shi' },
+  { email: 'n2rowc@tamu.edu', first_name: 'nicholas', last_name: 'tuorci' },
+  { email: 'coopercalk@tamu.edu', first_name: 'cooper', last_name: 'calk' },
+  { email: 'jordandary@tamu.edu', first_name: 'jordan', last_name: 'daryanani' },
+  { email: 'vivek.somarapu@tamu.edu', first_name: 'vivek', last_name: 'somarapu' },
+  { email: 'dhruvmanihar@tamu.edu', first_name: 'dhruv', last_name: 'manihar' }
+]
 
-# teachers = teachers_data.map do |teacher_data|
-#   Teacher.find_or_create_by!(email: teacher_data[:email]) do |teacher|
-#     teacher.name = "#{teacher_data[:first_name].capitalize} #{teacher_data[:last_name].capitalize}"
-#   end
-# end
+teachers = teachers_data.map do |teacher_data|
+  Teacher.find_or_create_by!(email: teacher_data[:email]) do |teacher|
+    teacher.name = "#{teacher_data[:first_name].capitalize} #{teacher_data[:last_name].capitalize}"
+  end
+end
 
-# # Seed Students
-# 200.times do
-#   assigned_teacher = teachers.sample
-#   assigned_semester = semesters.sample
-#   Student.create!(
-#     first_name: Faker::Name.first_name,
-#     last_name: Faker::Name.last_name,
-#     email: Faker::Internet.unique.email,
-#     uin: Faker::Number.number(digits: 9).to_i,
-#     teacher: assigned_teacher,
-#     teacher_id: assigned_teacher.id,
-#     authenticate: [true, false].sample,
-#     semester: assigned_semester,
-#     semester_id: assigned_semester.id
-#   )
-# end
+# Seed Students
+200.times do
+  assigned_teacher = teachers.sample
+  assigned_semester = semesters.sample
+  Student.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.unique.email,
+    uin: Faker::Number.number(digits: 9).to_i,
+    teacher: assigned_teacher,
+    teacher_id: assigned_teacher.id,
+    authenticate: [true, false].sample,
+    semester: assigned_semester,
+    semester_id: assigned_semester.id
+  )
+end
 
 #----------------------------------------------
 # Dynamic Answer Seeding Using Generator Mapping
@@ -131,7 +131,7 @@ Student.find_each do |student|
         category: category,
         question_description: generated_question[:question],
         answer_choices: (generated_question[:answer_choices] || ['TBD']).to_json,
-        answer: 'test answer',       # New placeholder answer text.
+        answer: 'Seeded Answer',       # New placeholder answer text.
         correctness: rand < 0.5,       # Randomly assigns true or false.
         student_email: student.email,
         date_completed: Faker::Date.backward(days: 30),
